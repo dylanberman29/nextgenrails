80d33c464558031db85e4d3d2cd64fa079306f86
Dylan <bermandylan6@gmail.com>
2019-08-09 12:23:06 -0400

    more new stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :volunteerheres
   get "becomevolunteer" => "volunteerheres#becomevolunteer"
   get "findvolunteers" => "volunteerheres#findvolunteer"
   get "contact" => "volunteerheres#contact"
+
+  root "volunteerheres#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
